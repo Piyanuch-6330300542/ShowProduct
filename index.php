f31288a264c13c9183dec6c7eddddd5ba3132b12
Piyanuch-6330300542 <77092780+Piyanuch-6330300542@users.noreply.github.com>
2022-09-20 23:53:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
-// Connect to DB
-$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 $per_page=5;
 if(isset($_GET["page"])) $start_page=$_GET["page"]*$per_page;
 else $start_page=0;
-
+// Connect to DB
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if(!$con) die("Connnect mysql database fail!!".mysqli_connect_error());
 echo "Connect mysql successfully!";
 $sql="SELECT * FROM product";
@@ -22,7 +22,7 @@ $numrow=mysqli_num_rows($result);
 echo "<br>".$numrow." Records<br>";
 for($i=0;$i<ceil($numrow/$per_page);$i++)
     echo "<a href='show_product.php?page=$i'>[".($i+1)."]</a>";
-$sql="SELECT * FROM product2 LIMIT $start_page,$per_page";
+$sql="SELECT * FROM product LIMIT $start_page,$per_page";
 $result=mysqli_query($con,$sql);
 if(mysqli_num_rows($result)>0){
     echo "<table border=1><tr><th>id</th><th>name</th><th>description</th><th>price</th><th></th></tr>";
@@ -44,3 +44,4 @@ if(mysqli_num_rows($result)>0){
 }
 
 mysqli_close($con);
+?>
